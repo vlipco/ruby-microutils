f1472476b5da2c4efd8447bd06d50b0fe277373b
David Pelaez <david@vlipco.co>
2015-07-21 12:31:22 -0400

    added share code, gemspec still incomplete

---
+++ spec/microutils/metrics.rb
@@ -0,0 +1,30 @@
+require 'em-http-request'
+
+Thread.new { EventMachine.run }
+
+module Metrics
+
+  # TODO attr_accessor to define when to sent metrics
+
+  # TODO auto append data
+
+  # TODO sensitive data filter
+
+  # TODO timer object that can be stopped upon completion to trigger event send
+
+  # TODO unify EM with rails logger
+  def self.track_in(queue, data={}, &block)
+    data = data.to_h
+    if block_given?
+      block_result = nil
+      ms = Time.measure do
+        block_result = block.call
+      end
+      data[:duration_ms] = ms
+    end
+    http = Keen.publish_async queue.to_s, data
+    http.callback { |response| $logger.info "Keen event successfuly sent to collection=#{queue}"}
+    http.errback { $logger.error "Failed to track event in collection=#{queue}"}
+    return block_result if block_given?
+  end
+end
